e8ffbe83545e2c7039b9d9e28df5487e0bef2cd2
Ari100tell <alex111544@ukr.net>
2013-11-19 19:49:58 +0200

    First commit with working program
    Program calculates integral and using for that Middle Rectangle Method.
    Program have GUI

---
+++ src/ImagePanel.java
@@ -0,0 +1,27 @@
+public class ImagePanel extends javax.swing.JFrame {
+    
+    public ImagePanel() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+        jLabel1 = new javax.swing.JLabel();
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("1.png")));
+        jLabel1.setText("Foreign Cat!!!");
+        jLabel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 102, 102), 1, true));
+        getContentPane().add(jLabel1, java.awt.BorderLayout.CENTER);
+        pack();
+    }
+    
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ImagePanel().setVisible(true);
+            }
+        });
+    }
+    
+    private javax.swing.JLabel jLabel1;
+}
---
+++ src/RectangleMethod.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ari100tell
+ */
+public class RectangleMethod {
+public double run(double a, double b, int n )
+{
+   double result, h;
+   int i;
+ 
+   h = (b-a)/n; //Шаг сетки
+   result = 0.0;
+ 
+   for(i=1; i <= n; i++){
+      result += calculateFunction( a + h * (i - 0.5) ); //Вычисляем в средней точке и добавляем в сумму
+   }
+   result *= h;
+ 
+   return result;
+}    
+public double calculateFunction(double x){
+    return Math.pow(x, 2)/Math.pow((2*x+0.3), 2);
+    
+}
+        
+}
